840091412af9fef7192a95328a69055b49a88f76
ezhumalaisanjay <ezhumalaisanjay05@gmail.com>
2024-09-23 20:40:52 +0530

    add

--- src/components/dashboards/default/EmployeeTable.js
+++ src/components/dashboards/default/EmployeeTable.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { Table, Button, Form, Image } from 'react-bootstrap'; // Import Image from react-bootstrap
+import { Table, Button, Form, Image } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import EmployeeForm from './EmployeeForm';
+import './EmployeeTable.css'; // Import a CSS file for custom styles
 
 const EmployeeTable = ({ employeeList, addEmployee }) => {
   const [selectedEmployees, setSelectedEmployees] = useState([]);
@@ -30,23 +31,12 @@ const EmployeeTable = ({ employeeList, addEmployee }) => {
       </div>
 
       <div className="d-flex justify-content-between mb-3">
-        <div>
-          <Button
-            variant="primary"
-            className="btn btn-falcon-default btn-sm"
-            onClick={handleNewButtonClick}
-          >
-            + New
-          </Button>
-        </div>
-        <div>
-          <Button
-            variant="secondary"
-            className="ms-2 btn btn-falcon-default btn-sm"
-          >
-            Export
-          </Button>
-        </div>
+        <Button variant="primary" onClick={handleNewButtonClick}>
+          + New
+        </Button>
+        <Button variant="secondary" className="ms-2">
+          Export
+        </Button>
       </div>
 
       {showForm ? (
@@ -55,87 +45,80 @@ const EmployeeTable = ({ employeeList, addEmployee }) => {
         </div>
       ) : (
         <>
-          <Table striped bordered hover>
-            <thead>
-              <tr>
-                <th>
-                  <Form.Check
-                    type="checkbox"
-                    onChange={e => {
-                      if (e.target.checked) {
-                        setSelectedEmployees(employeeList);
-                      } else {
-                        setSelectedEmployees([]);
-                      }
-                    }}
-                  />
-                </th>
-                <th>Profile</th>
-                <th>Employee ID</th>
-                <th>Name</th>
-                <th>Date of Birth</th>
-                <th>Address</th>
-                <th>Phone</th>
-                <th>Email</th>
-              </tr>
-            </thead>
-            <tbody>
-              {employeeList.map((employee, index) => (
-                <tr
-                  key={index}
-                  style={{ cursor: 'pointer' }}
-                  className="align-middle white-space-nowrap"
-                  role="row"
-                >
-                  <td>
+          <div className="table-responsive">
+            {' '}
+            {/* Make table responsive */}
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>
                     <Form.Check
                       type="checkbox"
-                      checked={selectedEmployees.includes(employee)}
-                      onChange={() => toggleEmployeeSelection(employee)}
+                      onChange={e => {
+                        if (e.target.checked) {
+                          setSelectedEmployees(employeeList);
+                        } else {
+                          setSelectedEmployees([]);
+                        }
+                      }}
                     />
-                  </td>
-                  <td>
-                    <Image
-                      src={employee.profileImage}
-                      alt={`${employee.name}'s profile`}
-                      roundedCircle
-                      width={50}
-                      height={50}
-                    />
-                  </td>
-                  <td>{employee.employeeId}</td>
-                  <td>{employee.name}</td>
-                  <td>{employee.dateOfBirth}</td>
-                  <td>{employee.address}</td>
-                  <td>{employee.phone}</td>
-                  <td>{employee.email}</td>
+                  </th>
+                  <th>Profile</th>
+                  <th>Employee ID</th>
+                  <th>Name</th>
+                  <th>Date of Birth</th>
+                  <th>Address</th>
+                  <th>Phone</th>
+                  <th>Email</th>
                 </tr>
-              ))}
-            </tbody>
-          </Table>
+              </thead>
+              <tbody>
+                {employeeList.map((employee, index) => (
+                  <tr
+                    key={index}
+                    style={{ cursor: 'pointer' }}
+                    className="align-middle white-space-nowrap"
+                    role="row"
+                  >
+                    <td>
+                      <Form.Check
+                        type="checkbox"
+                        checked={selectedEmployees.includes(employee)}
+                        onChange={() => toggleEmployeeSelection(employee)}
+                      />
+                    </td>
+                    <td>
+                      <Image
+                        src={employee.profileImage}
+                        alt={`${employee.name}'s profile`}
+                        roundedCircle
+                        width={50}
+                        height={50}
+                      />
+                    </td>
+                    <td>{employee.employeeId}</td>
+                    <td>{employee.name}</td>
+                    <td>{employee.dateOfBirth}</td>
+                    <td>{employee.address}</td>
+                    <td>{employee.phone}</td>
+                    <td>{employee.email}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </div>
 
           <p>Selected Employees: {selectedEmployees.length}</p>
 
           <div className="card-footer">
             <div className="d-flex justify-content-center align-items-center">
-              <Button
-                variant="secondary"
-                className="btn btn-falcon-default btn-sm m-2"
-              >
+              <Button variant="secondary" className="m-2">
                 Previous
               </Button>
-              <Button
-                variant="secondary"
-                className="active page me-2 btn btn-falcon-default btn-sm"
-              >
+              <Button variant="secondary" className="active page me-2">
                 Page 1
               </Button>
-              <Button
-                variant="secondary"
-                className="btn btn-falcon-default btn-sm"
-              >
-                Next
-              </Button>
+              <Button variant="secondary">Next</Button>
             </div>
           </div>
         </>
@@ -153,7 +136,7 @@ EmployeeTable.propTypes = {
       address: PropTypes.string.isRequired,
       phone: PropTypes.string.isRequired,
       email: PropTypes.string.isRequired,
-      profileImage: PropTypes.string // New prop for profile image
+      profileImage: PropTypes.string
     })
   ).isRequired,
   addEmployee: PropTypes.func.isRequired
