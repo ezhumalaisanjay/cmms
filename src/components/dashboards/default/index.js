58076d9a4359c9b20b87256e8370a620e4c197db
ezhumalaisanjay <ezhumalaisanjay05@gmail.com>
2024-09-23 18:21:40 +0530

    add

--- src/components/dashboards/default/index.js
+++ src/components/dashboards/default/index.js
@@ -4,15 +4,56 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   const [employeeList, setEmployeeList] = useState([
-    { name: 'Anna', email: 'anna@example.com', age: 18 },
-    { name: 'Homer', email: 'homer@example.com', age: 35 },
-    { name: 'Oscar', email: 'oscar@example.com', age: 52 },
-    { name: 'Emily', email: 'emily@example.com', age: 30 },
-    { name: 'Jara', email: 'jara@example.com', age: 25 }
+    {
+      employeeId: '1',
+      name: 'Anna',
+      dateOfBirth: '2005-01-01',
+      address: '123 Main St',
+      phone: '123-456-7890',
+      email: 'anna@example.com'
+    },
+    {
+      employeeId: '2',
+      name: 'Homer',
+      dateOfBirth: '1988-01-01',
+      address: '456 Elm St',
+      phone: '234-567-8901',
+      email: 'homer@example.com'
+    },
+    {
+      employeeId: '3',
+      name: 'Oscar',
+      dateOfBirth: '1971-01-01',
+      address: '789 Oak St',
+      phone: '345-678-9012',
+      email: 'oscar@example.com'
+    },
+    {
+      employeeId: '4',
+      name: 'Emily',
+      dateOfBirth: '1993-01-01',
+      address: '101 Pine St',
+      phone: '456-789-0123',
+      email: 'emily@example.com'
+    },
+    {
+      employeeId: '5',
+      name: 'Jara',
+      dateOfBirth: '1998-01-01',
+      address: '202 Maple St',
+      phone: '567-890-1234',
+      email: 'jara@example.com'
+    }
   ]);
 
   const addEmployee = newEmployee => {
-    setEmployeeList([...employeeList, newEmployee]);
+    setEmployeeList([
+      ...employeeList,
+      {
+        ...newEmployee,
+        employeeId: (employeeList.length + 1).toString()
+      }
+    ]);
   };
 
   return (
